d5bbdaa028510c2d9e76e8c6be8f01fd714ebc0e
Shubham More <shubham.more@joshsoftware.com>
2023-01-04 10:44:25 +0530

    Reusdable modal component added

---
+++ src/Shared/components/Modal.tsx
@@ -0,0 +1,34 @@
+interface IProps {
+  isOpen: boolean;
+  title: string;
+  body: string;
+  btn1: string;
+  btn2: string;
+  btn1Handler: any;
+  btn2Handler: () => void;
+}
+const Modal = (props: IProps) => {
+  const { isOpen, title, body, btn1, btn2, btn1Handler, btn2Handler } = props;
+  return (
+    <>
+      {isOpen ? (
+        <div className="modalBackground">
+          <div className="modal-container">
+            <div className="title">
+              <h2>{title}</h2>
+            </div>
+            <div className="body">
+              <p>{body}</p>
+            </div>
+            <div className="footer">
+              <button onClick={btn1Handler}>{btn1}</button>
+              <button onClick={btn2Handler}>{btn2}</button>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+export default Modal;
