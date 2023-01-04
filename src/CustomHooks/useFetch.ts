8b9bb20ac6698a68278e83cd1606af69e5c52853
Shubham More <shubham.more@joshsoftware.com>
2023-01-04 12:30:48 +0530

    Modularized the argument type

--- src/CustomHooks/useFetch.ts
+++ src/CustomHooks/useFetch.ts
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
+import { TODO_URL } from "../utils/constant";
 
-export const useFetch = (url: any) => {
+export const useFetch = (url?: string) => {
   const [data, setData] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>("");
 
+  let newUrl=url?TODO_URL+"/"+url:TODO_URL;
+  
   useEffect(() => {
-    fetch(url)
+    fetch(newUrl)
       .then((res) => {
         if (!res.ok) throw new Error("Could not fetch data");
         return res.json();
