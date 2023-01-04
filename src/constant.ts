7e0b65e54a37b8fba7024bc022f6d2b51bc7d2d3
Shubham More <shubham.more@joshsoftware.com>
2023-01-04 12:36:15 +0530

    Constant and data file

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,9 @@
+import React,{ChangeEvent} from "react"
 export const TASK_STATE={
     PENDING:"Pending",
     COMPLETED:"Completed"
 }
+
+// export const EVENT_TYPE={
+//     CHANGE: ChangeEvent<HTMLSelectElement>
+// }
