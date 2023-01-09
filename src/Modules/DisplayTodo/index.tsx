44a919daccfd6f72b48cfa44c0d04da1e50a6ce5
Shubham More <shubham.more@joshsoftware.com>
2023-01-09 18:24:25 +0530

    Removed the unnecessary code

--- src/Modules/DisplayTodo/index.tsx
+++ src/Modules/DisplayTodo/index.tsx
@@ -24,33 +24,30 @@ const TodoContainer = (): ReactElement => {
   let displayTodos = todos;
 
   useEffect(() => {
-    if (!!data) {
+    if (data) {
       setTodos(data);
     }
   }, [data]);
 
-  const markStatus = useCallback(
-    (id: number, completed: boolean): ToDoData[] =>
-      todos.map((todo) => {
-        if (todo.id === id) {
-          let tempObj = { ...todo, completed: completed };
-          fetch(`${TODO_URL}/${id}`, {
-            method: "PATCH",
-            mode: "cors",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(tempObj),
-          })
-            .then((res) => res.json())
-            .catch((err) => err);
-          return tempObj;
-        } else {
-          return todo;
-        }
-      }),
-    [todos]
-  );
+  const markStatus = (id: number, completed: boolean): ToDoData[] =>
+    todos.map((todo) => {
+      if (todo.id === id) {
+        let tempObj = { ...todo, completed: completed };
+        fetch(`${TODO_URL}/${id}`, {
+          method: "PATCH",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(tempObj),
+        })
+          .then((res) => res.json())
+          .catch((err) => err);
+        return tempObj;
+      } else {
+        return todo;
+      }
+    });
 
   const markTodoCompleted = useCallback(
     (id: number, completed: boolean) => setTodos(markStatus(id, completed)),
