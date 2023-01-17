71bbc586f4c62adeec1efac7430e7d53170008c1
Shubham More <shubham.more@joshsoftware.com>
2023-01-17 16:26:37 +0530

    Modified SelectBox and SearchBox options array

--- src/Shared/components/SearchBox.tsx
+++ src/Shared/components/SearchBox.tsx
@@ -1,12 +1,13 @@
-import { ReactElement } from "react";
+import React, { ReactElement } from "react";
 
 import { InputChangeEvent } from "../../constant";
 
 interface IProps {
+  searchInput: string;
   searchHandler: (e: InputChangeEvent) => void;
 }
 const SearchBox: React.FC<IProps> = (props): ReactElement => {
-  const { searchHandler } = props;
+  const { searchInput, searchHandler } = props;
 
   return (
     <div className="search-box-container">
@@ -15,10 +16,11 @@ const SearchBox: React.FC<IProps> = (props): ReactElement => {
         className="search-box"
         type="text"
         placeholder="Search"
+        value={searchInput}
         onChange={searchHandler}
       />
     </div>
   );
 };
 
-export default SearchBox;
+export default React.memo(SearchBox);
--- src/Shared/components/SelectBox.tsx
+++ src/Shared/components/SelectBox.tsx
@@ -2,20 +2,35 @@ import React from "react";
 
 import { SelectChangeEvent } from "../../constant";
 
+interface optionType {
+  key: string | undefined;
+  value: string;
+}
 interface IProps {
-  selectHandler: (e: SelectChangeEvent) => void;
-  options: string[];
+  selectHandler: (e: string) => void;
+  options: optionType[];
+  value: string;
 }
 
 const SelectBox = (props: IProps): React.ReactElement => {
-  const { selectHandler, options } = props;
+  const { selectHandler, options, value } = props;
+
+  const selectFunction = (e: SelectChangeEvent) => {
+    let data = e.target.value;
+    selectHandler(data);
+  };
 
   return (
     <div className="select-box-container">
-      <select name="select-input" id="select-input" onChange={selectHandler}>
-        {options.map((option: string) => (
-          <option value={option} key={option}>
-            {option}
+      <select
+        value={value}
+        name="select-input"
+        id="select-input"
+        onChange={selectFunction}
+      >
+        {options.map((option, idx) => (
+          <option value={option.key} key={idx}>
+            {option.value}
           </option>
         ))}
       </select>
@@ -23,4 +38,4 @@ const SelectBox = (props: IProps): React.ReactElement => {
   );
 };
 
-export default SelectBox;
+export default React.memo(SelectBox);
